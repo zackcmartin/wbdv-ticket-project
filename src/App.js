9a9e905e7632be01cd2911aa7ff02ee4657fe24b
Zack Martin <33234759+zackcmartin@users.noreply.github.com>
2019-11-11 20:27:48 -0500

    fixed import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import TicketPage from './pages/TicketPage'
+import StubHubController from './components/StubHubController'
 
 function App() {
   return (
